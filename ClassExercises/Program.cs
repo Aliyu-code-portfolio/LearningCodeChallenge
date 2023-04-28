74d392935a68d787f7c4e8af8ddf61201933d493
Aliyu Abdullahi <aliyuabdullahi074@gmail.com>
2023-04-28 14:18:16 +0100

    Add project files.

---
+++ ClassExercises/Program.cs
@@ -0,0 +1,23 @@
+﻿
+using ClassExercises;
+bool loop=true;
+while (loop)
+{
+    Console.WriteLine("Enter a sentence to analyse");
+    string? sentence = Console.ReadLine();
+    VowelConsonantCount vc = new(sentence);
+    vc.GetVowelConsonantDetails();
+    Console.WriteLine();
+    Console.WriteLine("Do you want to test another sentence? NO/YES");
+    string choice = Console.ReadLine();
+    switch (choice.ToLower())
+    {
+        case "yes":
+            
+            break;
+        case "no":
+        default:
+            loop = false;
+            break;
+    }
+}
---
+++ ClassExercises/VowelConsonantCount.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ClassExercises
+{
+    public class VowelConsonantCount
+    {
+        private string sentence;
+        public VowelConsonantCount(string sentence)
+        {
+
+            this.sentence = sentence;
+
+        }
+        private void IgnoreCaseAndSymbols()
+        {
+            sentence=sentence.ToLower().Trim();
+            sentence = Regex.Replace(sentence, "[^a-zA-Z0-9]", "", RegexOptions.Compiled);
+            
+        }
+        private double Percentage(double numberOfOccurance)
+        {
+            return (numberOfOccurance / sentence.Length) * 100;
+        }
+        public void GetVowelConsonantDetails()
+        {
+            Dictionary<char,int> wordCount = new Dictionary<char,int>();
+            char[] vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
+            int vowelCount = 0;
+            int consonantCount = 0;
+            IgnoreCaseAndSymbols();
+            foreach (char c in sentence)
+            {
+                if (vowels.Contains(c))
+                {
+                    vowelCount++;
+                }
+                else
+                {
+                    consonantCount++;
+                }
+
+                if (wordCount.ContainsKey(c))
+                {
+                    wordCount[c]++;
+                }
+                else
+                {
+                    wordCount.Add(c, 1);
+                    
+                }
+            }
+            PrintDetailToScreen(vowelCount, consonantCount,vowels, wordCount);
+        }
+
+        private void PrintDetailToScreen(int vowelCount, int consonantCount, char[] vowels, Dictionary<char, int> wordCount)
+        {
+            //List<char> sortedwords= new List<char> { wordCount.Keys.ToList().Sort()};
+            Console.WriteLine("Details of your sentence is as follows");
+            Console.WriteLine("Total word count is: " + sentence.Length);
+            Console.WriteLine();
+            Console.WriteLine("Vowels count is: "+  vowelCount);
+            Console.WriteLine("Consonant count is: " + consonantCount);
+            Console.WriteLine();
+            Console.WriteLine("Each alphabet appearance count is:");
+            foreach (char c in wordCount.Keys)
+            {
+                Console.WriteLine($"{c}: {wordCount[c]}({Percentage(wordCount[c]):F2}%)");
+            }
+           // Console.WriteLine(wordCount.OrderBy)
+        }
+    }
+}
